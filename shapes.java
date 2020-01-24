ba4ae56f5c3e9db32418c970c6b88916c0a02ed7
anshpatel3137 <60273561+anshpatel3137@users.noreply.github.com>
2020-01-24 15:23:41 -0500

    Initial Commit

---
+++ shapes.java
@@ -0,0 +1,24 @@
+//Ansh Patel
+//9-10-2019
+//Description: crating different objects
+
+public class shapes
+{
+
+	public static void main(String Args[])
+	{
+
+		 
+	System.out.println("*********      ***        *      *");       
+        System.out.println("*       *    *     *     ***    * *");               
+	System.out.println("*       *   *       *   *****  *   *");
+	System.out.println("*       *   *       *     *   *     *");
+	System.out.println("*       *   *       *     *  *       *");     
+	System.out.println("*       *   *       *     * *         *");
+	System.out.println("*       *   *       *     *  *       *");
+        System.out.println("*       *   *       *     *   *     *");
+        System.out.println("*       *    *     *      *    *   *");
+	System.out.println("*********      ***        *      *");
+	}		 
+
+}
